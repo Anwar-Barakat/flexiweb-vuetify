36bda63496a74950fbc234ad018d1e4f21c27313
Anwar-Barakat <brkatanwar0@gmail.com>
2024-11-26 22:56:36 +0400

    feat: add SCSS styles and global styles for application layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
 import "./style.css";
+
+// Import the App component
 import App from "./App.vue";
 
+// Import the global styles
+import './scss/main.scss';
+
+// Import the Vuetify plugin
 import vuetify from "./plugins/vuetify";
 
+// Create a new Vue app instance
 createApp(App).use(vuetify).mount("#app");
