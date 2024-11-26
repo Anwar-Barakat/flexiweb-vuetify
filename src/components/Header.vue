f464b8d7ba3b678ae3eddfed19ebe2d3016a2f8c
Anwar-Barakat <brkatanwar0@gmail.com>
2024-11-26 22:50:04 +0400

    refactor: restructure app layout and remove HelloWorld component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-toolbar
+      color="white"
+      >
+      <v-toolbar-title>PhotoGraphy</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text> Home </v-btn>
+
+        <v-btn text> About </v-btn>
+
+        <v-btn text> Contact </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
